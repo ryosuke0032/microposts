95a12c64ce05aee51c2330a60f77b1790d7362bb
ryosuke0032 <ryosuke0032@gmail.com>
2015-08-29 02:13:44 +0000

    kadai 1kaime

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show,:edit,:update]
   
   def show
-   @user = User.find(params[:id])    
   end
   
   def new
     @user = User.new
+  end
+  
+  def edit
+  end
+  
+  def update
+   if @user.update(user_params)
+      flash[:success] = "プロフィールを編集しました"
+      redirect_to @user
+    else
+      render 'edit'
+   end
   end
   
   def create
@@ -22,7 +34,10 @@ class UsersController < ApplicationController
 private
 
   def user_params
-	 params.require(:user).permit(:name,:email,:password,:password_confirmation)
+	 params.require(:user).permit(:name,:email,:password,:password_confirmation,:description,:location )
   end
 
+  def set_user
+   @user = User.find(params[:id]) 
+  end
 end
---
+++ db/migrate/20150829004023_add_details_to_user.rb
@@ -0,0 +1,6 @@
+class AddDetailsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :description, :string
+    add_column :users, :location, :string
+  end
+end
