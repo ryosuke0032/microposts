6c5d335eeb3a0b1b18338670a2ecc9f1f70320f8
ryosuke0032 <ryosuke0032@gmail.com>
2015-09-09 14:09:12 +0000

    zenkaku keshita

--- db/migrate/20150905074643_create_favorites.rb
+++ db/migrate/20150905074643_create_favorites.rb
@@ -1,12 +1,10 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
-    　t.integer :favoriter_id, index: true, foreign_key: true
+      t.integer :favoriter_id, index: true, foreign_key: true
       t.integer :favoritepost_id, index: true, foreign_key: true
-
       t.timestamps null: false
       t.index [:favoriter_id, :favoritepost_id], unique: true
-      
     end
   end
 end
